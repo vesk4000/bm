e919f3f0fc11c1769021984a8155512219a9a124
bobgle <lw.leonweber@gmail.com>
2015-12-14 17:41:16 +0100

    Added Social Share Buttons

---
+++ _site/getting-started.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>What Is Bitcoin – Learn all about crypto-currency.</title>
+<meta name="description" content="Anything">
+<meta charset="utf-8"/>
+<meta content='text/html; charset=utf-8' http-equiv='Content-Type'>
+<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0'>
+<meta name="description" content="Learn all about crypto-currency.">
+<meta property="og:description" content="Learn all about crypto-currency."/>
+<meta name="author" content="What Is Bitcoin"/>
+<!--[if lt IE 9]>
+      <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+<link rel="stylesheet" type="text/css" href="/style.css"/>
+<link rel="alternate" type="application/rss+xml" title="What Is Bitcoin - Learn all about crypto-currency." href="/feed.xml"/>
+ 
+<script type='text/javascript'>
+    var googletag = googletag || {};
+    googletag.cmd = googletag.cmd || [];
+    (function() {
+    var gads = document.createElement('script');
+    gads.async = true;
+    gads.type = 'text/javascript';
+    var useSSL = 'https:' == document.location.protocol;
+    gads.src = (useSSL ? 'https:' : 'http:') +
+      '//www.googletagservices.com/tag/js/gpt.js';
+    var node = document.getElementsByTagName('script')[0];
+    node.parentNode.insertBefore(gads, node);
+    })();
+</script>
+<script type='text/javascript'>
+
+    googletag.cmd.push(function() {
+        var mapping = googletag.sizeMapping().
+          addSize([980, 690], [728, 90]).
+          addSize([0, 0], [320, 50]).
+          build();
+        googletag.defineSlot('/94788221/wucfooter', [[320,50],[728, 90]], 'div-gpt-ad-1429910991777-0').defineSizeMapping(mapping).addService(googletag.pubads());
+        googletag.pubads().enableSingleRequest();
+        googletag.pubads().collapseEmptyDivs();
+        googletag.enableServices();
+    });
+</script>
+</head>
+<body>
+<div class="wrapper-masthead">
+<div class="container">
+<header class="masthead clearfix">
+<a href="/" class="site-avatar"><img src="/images/we-use-coins-what-is-bitcoin.png"/></a>
+<div class="site-info">
+<h1 class="site-name"><a href="/">What Is Bitcoin</a></h1>
+<p class="site-description">Learn all about crypto-currency.</p>
+</div>
+<nav>
+<a href="/en/getting-started/">Start</a>
+<a href="/en/get-free-bitcoins/">Free Bitcoins</a>
+<a href="/en/bitcoin-expert-interviews/">Interviews</a>
+<a href="/en/questions/">FAQ</a>
+<a href="/en/whos-who/">Who's Who</a>
+<a href="/en/mining-guide/">Mining</a>
+<a href="/en/alt-coins/">Alt-coins</a>
+</nav>
+</header>
+</div>
+</div>
+<div id="main" role="main" class="container">
+<link rel="canonical" href="https://www.weusecoins.com/en/getting-started/"/>
+<title>Getting Started with Bitcoin - We Use Coins</title>
+<meta name="description" content="What is Bitcoin? Get started with Bitcoin right. Learn how to buy bitcoins, use the best bitcoin wallets and find the best bitcoin exchanges.">
+<h2><b>Step 1</b> ---> How to get a Bitcoin wallet</h2>
+<p>Setting up your first Bitcoin wallet is simple with several quality mobile apps available.</p>
+<p>A great path would be <b>(1)</b> a mobile wallet like Airbitz (iPhone, Android), Breadwallet (iPhone) or Mycelium (Android), <b>(2)</b> acquire bitcoins from a friend, as payment for a good or service or buy bitcoins from Circle and then <b>(3)</b> if you decide to store large amounts of bitcoins and want to know they are absolutely safe then use Armory.</p>
+<p><h3>Best Mobile Bitcoin Wallets</h3>
+iPhone and Android apps are available with popular choices including:</p>
+<ul>
+<li><a href="https://airbitz.co">Airbitz</a> (Android, iPhone)</li>
+<li><a href="http://breadwallet.com/">Breadwallet</a> (iPhone)</li>
+<li><a href="https://play.google.com/store/apps/details?id=com.mycelium.wallet">Mycelium</a> (Android)</li>
+</ul>
+<p><h3>Best Bitcoin Software Wallets</h3>
+Bitcoin software wallets are downloaded to your computer, give you more control and do not depend on third-party services. Most software bitcoin wallets require a day or so to download the blockchain before they are ready for use.</p>
+<ul>
+<li><a href="http://bitcoin.org/en/download">Bitcoin Core</a> - The best option for network security.</li>
+<li><a href="https://bitcoinarmory.com/">Armory</a> - (Highly Recommended) Armory is the most mature, secure and full featured Bitcoin wallet but it can be technologically intimidating for users. Whether you are an individual storing $1,000 or institution storing $1,000,000,000 this is the most secure option available. Users are in complete control all Bitcoin private keys and can setup a secure offline-signing process in Armory.</li>
+</ul>
+<p><h3>Best Bitcoin Web Wallets</h3>
+Bitcoin web wallets are the easiest and most convenient to use but are potentially less secure than the above options because the private keys to your bitcoins are usually held by a third-party.</p>
+<ul>
+<li><a href="https://www.circle.com/signup">Circle</a></li>
+</ul>
+<p><h3>Best Bitcoin Hardware Wallets</h3>
+Bitcoin hardware wallets are the most secure because they do not expose your private keys to the network.</p>
+<ul>
+<li><a href="https://www.ledgerwallet.com/r/4727">Ledger Wallet</a> - Smartcard based and malware proof | <a href="/bitcoin-ledger-wallet-review/">Ledger Wallet Review</a></li>
+</ul>
+<h2><b>Step 2 </b> ---> How to get Bitcoins</h2>
+<p><h3>How to buy Bitcoins</h3>
+With the <a href="/en/bitcoin-price/">Bitcoin price</a> so volatile many people seem curious about getting some. Buying bitcoins is getting simpler every day. Here are our recommendations:</p>
+<ul>
+<li><a href="http://www.runtogold.com/localbitcoins">Local Bitcoins</a> - This fantastic service allows you to search for people in your community willing to sell bitcoins to you directly. But be careful!</li>
+<li><a href="https://www.circle.com/en">Circle</a> - An easy to use bitcoin broker to link with your bank account</li>
+<li><a href="https://www.kraken.com/">Kraken</a> - The largest European exchange with same-day SEPA</li>
+</ul>
+<p><h3>How To Earn Bitcoins</h3>
+Many people find that the best way to get bitcoins is to accept them as payment for the products or services they already sell. Just give your customers the option to pay in bitcoin.</p>
+<p>To make it easy, you may want to sign up for a bitcoin merchant account which allows you to send invoices and integrate bitcoin payments into your order pages for the most popular online shopping cart systems.</p>
+<ul>
+<li><a href="/en/get-free-bitcoins/">WeUseCoins Bitcoin Faucet</a> - Get free bitcoins that are provided by sponsors</li>
+<li><a href="https://bitpay.com/">BitPay</a> - (Highly Recommended) - Merchant tools and account services</li>
+<li><a href="http://www.reddit.com/r/Jobs4bitcoins">Jobs 4 Bitcoin</a> - Subreddit where you can find jobs</li>
+</ul>
+<p><h3>What Is Bitcoin Mining?</h3>
+Mining bitcoins requires an investment in specialized bitcoin mining hardware designed to process double round sha256 hash verifications at high speed. Getting started with Bitcoin mining can be a daunting endeavor and it has become highly specialized so you may want to leave it to professionals. Nevertheless, there are many excellent bitcoin mining systems available.</p>
+<p><em>It is important to point out that bitcoin mining is highly competitive and risky for would-be participants</em>. Please see our <a href="/en/mining-guide">bitcoin mining</a> guide for more detail.</p>
+<h2><b>Step 3</b> ---> How to use bitcoins?</h2>
+<p>Starting to use bitcoins can be exciting because it is a new technology. Bitcoins can be used to purchase just about anything you want. Dental services, a brand new car, travel and even luxury real estate.</p>
+<p>We recommend searching the Bitcoin merchant directories below to locate the merchants you are interested in. You can buy gift cards with bitcoin and shop easily at most of the stores you already use.</p>
+<p><h3>Bitcoin Merchant Directories</h3>
+<ul>
+<li><a href="https://airbitz.co/search">Airbitz</a> - Find bitcoin friendly merchants near you and online</li>
+<li><a href="http://coinmap.org/">Coinmap</a> - Find bitcoin friendly merchants near you</li>
+<li><a href="http://www.howtovanish.com/purseio">Purse.io</a> - Get discounts greater than 20% on Amazon.com</li>
+</ul>
+<p><h3>Buy Gift Cards With Bitcoins</h3>
+Available from Gyft and spendable at:</p>
+<ul>
+<li><a href="https://www.gyft.com/buy-gift-cards/amazon-com/">Amazon</a></li>
+</ul>
+<p><h3>Charity</h3>
+<ul>
+<li>We Use Coins - Donations greatly appreciated: 1CXiG6hZmMWUsr6sgF3Tx5nfKj1FN8dU1c </li>
+<li><a href="http://bitgivefoundation.org/">BitGive</a> - a non-profit foundation that is accepting donations from the Bitcoin community and providing charitable gifts to environmental and public health causes worldwide.</li>
+</ul>
+<h2><b>Step 4</b> ---> Keeping up to date with Bitcoin news</h2>
+Michael Casey and Paul Vigna of the Wall Street Journal authored <a href="http://www.runtogold.com/ageofcryptocurrency">The Age of Cryptocurrency</a> which has become a best seller on the topic of Bitcoin. This may be a great book for you to start with when forming an intellectual foundation on this new technology.
+<p>Here are some good places to keep up with bitcoin news and discussion:</p>
+<ul>
+<li><a href="http://www.reddit.com/r/bitcoin">r/bitcoin</a> - Bitcoin links and discussion by the Reddit community</li>
+<li><a href="https://www.ybitcoin.com/">yBitcoin</a> - An excellent source for beginners</li>
+<li><a href="https://bitcoinmagazine.com/">Bitcoin Magazine</a> - Bitcoin news with insightful articles</li>
+<li><a href="https://www.youtube.com/watch?v=JP9-lAYngi4&amp;list=PLZvSjJphwk_Wk1SYkECKUxCdq5aOzmmhf">The Video</a> - An excellent primer for anyone curious to understand the bitcoin phenomenon</li>
+</ul>
+</div>
+<div class="wrapper-footer">
+<div class="container">
+<footer class="footer">
+ 
+<div id='div-gpt-ad-1429910991777-0' class="dfp">
+<script type='text/javascript'>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1429910991777-0'); });
+</script>
+</div>
+ 
+<style type="text/css">#af-form-1839974038 .af-body .af-textWrap{width:98%;display:block;float:none;}#af-form-1839974038 .af-body input.text,#af-form-1839974038 .af-body textarea{background-color:#FFFFFF;border-color:#919191;border-width:1px;border-style:solid;color:#000000;text-decoration:none;font-style:normal;font-weight:normal;font-size:14px;font-family:Verdana,sans-serif;}#af-form-1839974038 .af-body input.text:focus,#af-form-1839974038 .af-body textarea:focus{background-color:#FFFAD6;border-color:#030303;border-width:1px;border-style:solid;}#af-form-1839974038 .af-body label.previewLabel{display:block;float:none;text-align:left;width:auto;color:#FFFFFF;text-decoration:none;font-style:normal;font-weight:normal;font-size:18px;font-family:Verdana,sans-serif;}#af-form-1839974038 .af-body{padding-bottom:15px;padding-top:15px;background-repeat:no-repeat;background-position:inherit;background-image:none;color:#000000;font-size:10px;font-family:Verdana,sans-serif;}#af-form-1839974038 .af-quirksMode{padding-right:15px;padding-left:15px;}#af-form-1839974038 .af-standards .af-element{padding-right:15px;padding-left:15px;}#af-form-1839974038 .buttonContainer input.submit{background-image:url("https://forms.aweber.com/images/auto/gradient/button/aaa.png");background-position:top left;background-repeat:repeat-x;background-color:#8a8a8a;border:1px solid #8a8a8a;color:#000000;text-decoration:none;font-style:normal;font-weight:normal;font-size:18px;font-family:Verdana,sans-serif;}#af-form-1839974038 .buttonContainer input.submit{width:auto;}#af-form-1839974038 .buttonContainer{text-align:center;}#af-form-1839974038 button,#af-form-1839974038 input,#af-form-1839974038 submit,#af-form-1839974038 textarea,#af-form-1839974038 select,#af-form-1839974038 label,#af-form-1839974038 optgroup,#af-form-1839974038 option{float:none;position:static;margin:0;}#af-form-1839974038 div{margin:0;}#af-form-1839974038 form,#af-form-1839974038 textarea,.af-form-wrapper,.af-form-close-button,#af-form-1839974038 img{float:none;color:inherit;position:static;background-color:none;border:none;margin:0;padding:0;}#af-form-1839974038 input,#af-form-1839974038 button,#af-form-1839974038 textarea,#af-form-1839974038 select{font-size:100%;}#af-form-1839974038 select,#af-form-1839974038 label,#af-form-1839974038 optgroup,#af-form-1839974038 option{padding:0;}#af-form-1839974038,#af-form-1839974038 .quirksMode{width:100%;max-width:302px;}#af-form-1839974038.af-quirksMode{overflow-x:hidden;}#af-form-1839974038{background-color:#000000;border-color:transparent;border-width:1px;border-style:none;}#af-form-1839974038{display:block;}#af-form-1839974038{overflow:hidden;}.af-body .af-textWrap{text-align:left;}.af-body input.image{border:none!important;}.af-body input.submit,.af-body input.image,.af-form .af-element input.button{float:none!important;}.af-body input.text{width:100%;float:none;padding:2px!important;}.af-body.af-standards input.submit{padding:4px 12px;}.af-clear{clear:both;}.af-element label{text-align:left;display:block;float:left;}.af-element{padding:5px 0;}.af-form-wrapper{text-indent:0;}.af-form{text-align:left;margin:auto;}.af-quirksMode .af-element{padding-left:0!important;padding-right:0!important;}.lbl-right .af-element label{text-align:right;}body{}</style>
+<form method="post" class="af-form-wrapper" accept-charset="UTF-8" action="https://www.aweber.com/scripts/addlead.pl">
+<div style="display: none;">
+<input type="hidden" name="meta_web_form_id" value="1839974038"/>
+<input type="hidden" name="meta_split_id" value=""/>
+<input type="hidden" name="listname" value="awlist3817081"/>
+<input type="hidden" name="redirect" value="" id="redirect_b88f42ac6387571da1a38e8c137dfed2"/>
+<input type="hidden" name="meta_adtracking" value="Tiny_Black"/>
+<input type="hidden" name="meta_message" value="1"/>
+<input type="hidden" name="meta_required" value="email"/>
+<input type="hidden" name="meta_tooltip" value="email||Learn More | Email Address"/>
+</div>
+<div id="af-form-1839974038" class="af-form"><div id="af-body-1839974038" class="af-body af-standards">
+<div class="af-element">
+<label class="previewLabel" for="awf_field-72499846"></label>
+<div class="af-textWrap"><input class="text" id="awf_field-72499846" type="text" name="email" value="Learn More | Email Address" tabindex="500" onfocus=" if (this.value == 'Learn More | Email Address') { this.value = ''; }" onblur="if (this.value == '') { this.value='Learn More | Email Address';} "/>
+</div><div class="af-clear"></div>
+</div>
+<div class="af-element buttonContainer">
+<input name="submit" class="submit" type="submit" value="Submit" tabindex="501"/>
+<div class="af-clear"></div>
+</div>
+</div>
+</div>
+<div style="display: none;"><img src="https://forms.aweber.com/form/displays.htm?id=jBzMnJzsLAzMHA==" alt=""/></div>
+</form>
+<script type="text/javascript">
+    <!--
+    (function() {
+        var IE = /*@cc_on!@*/false;
+        if (!IE) { return; }
+        if (document.compatMode && document.compatMode == 'BackCompat') {
+            if (document.getElementById("af-form-1839974038")) {
+                document.getElementById("af-form-1839974038").className = 'af-form af-quirksMode';
+            }
+            if (document.getElementById("af-body-1839974038")) {
+                document.getElementById("af-body-1839974038").className = "af-body inline af-quirksMode";
+            }
+            if (document.getElementById("af-header-1839974038")) {
+                document.getElementById("af-header-1839974038").className = "af-header af-quirksMode";
+            }
+            if (document.getElementById("af-footer-1839974038")) {
+                document.getElementById("af-footer-1839974038").className = "af-footer af-quirksMode";
+            }
+        }
+    })();
+    -->
+</script>
+<script type="text/javascript">document.getElementById('redirect_b88f42ac6387571da1a38e8c137dfed2').value = document.location;</script>
+ 
+<p>
+| <a href="/de/">Deutsch</a> |
+<a href="/es/">Español</a> |
+<a href="/fr/">Français</a> |
+<a href="/nl/">Nederlands</a> |
+<a href="/ja/">日本語</a> |
+<p>
+<a href="https://twitter.com/WeUseCoins"><i class="svg-icon twitter"></i></a>
+<a href="https://www.facebook.com/weusecoins"><i class="svg-icon facebook"></i></a>
+<a href="https://www.youtube.com/user/weusecoins"><i class="svg-icon youtube"></i></a>
+<a href="/cdn-cgi/l/email-protection#6900070f06291e0c1c1a0c0a0600071a470a0604"><i class="svg-icon email"></i></a>
+<a href="https://github.com/sunnankar/wuc-new"><i class="svg-icon github"></i></a>
+<p>
+| <a href="/en/translations/">Translations</a> |
+<a href="https://github.com/sunnankar/wuc-new/blob/gh-pages/README.md">Contribute</a> |
+<a href="/en/materials/">Materials</a> |
+<a href="/en/team/">Team</a> |
+<a href="/en/about/">About</a> |
+<p>
+© 2011-2015 Hesiod Services LLC | <a href="/en/terms/">Terms</a> | <a href="/en/privacy/">Privacy</a>
+<p>
+</footer>
+</div>
+</div>
+ 
+<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-22253606-1', 'auto');
+		ga('send', 'pageview', {
+		  'page': '/en/getting-started/index.html',
+		  'title': ''
+		});
+	</script>
+ 
+<script type="text/javascript">
+/* <![CDATA[ */
+(function(){try{var s,a,i,j,r,c,l=document.getElementsByTagName("a"),t=document.createElement("textarea");for(i=0;l.length-i;i++){try{a=l[i].getAttribute("href");if(a&&a.indexOf("/cdn-cgi/l/email-protection") > -1  && (a.length > 28)){s='';j=27+ 1 + a.indexOf("/cdn-cgi/l/email-protection");if (a.length > j) {r=parseInt(a.substr(j,2),16);for(j+=2;a.length>j&&a.substr(j,1)!='X';j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}j+=1;s+=a.substr(j,a.length-j);}t.innerHTML=s.replace(/</g,"&lt;").replace(/>/g,"&gt;");l[i].setAttribute("href","mailto:"+t.value);}}catch(e){}}}catch(e){}})();
+/* ]]> */
+</script>
+</body>
+</html>
